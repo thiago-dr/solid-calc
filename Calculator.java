5a0dc59fdf711c1a4ee4826f8eaef021b50d17f3
Thiago-DR <thiago.dr.1055@gmail.com>
2022-05-15 15:59:16 -0300

    Change calculate method to static

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,6 @@
 public class Calculator {
 
-    public double calculate(double num1, double num2, Operation operation) {
+    public static double calculate(double num1, double num2, Operation operation) {
         return operation.calc(num1, num2);
     }
 }
